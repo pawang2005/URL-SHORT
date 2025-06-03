819daaf304342f93de620319b81933e3c4c1b8e4
PawanKumar Gupta <157799966+pawang2005@users.noreply.github.com>
2025-06-03 09:32:13 +0530

    Delete scripts directory

--- scripts/after_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/URL-SHORT/deploy.log
-
-echo 'cd /home/ec2-user/URL-SHORT' >> /home/ec2-user/URL-SHORT/deploy.log
-cd /home/ec2-user/URL-SHORT >> /home/ec2-user/URL-SHORT/deploy.log
-
-echo 'npm install' >> /home/ec2-user/URL-SHORT/deploy.log 
-npm install >> /home/ec2-user/URL-SHORT/deploy.log
